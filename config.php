bd2b6607010ccb5056a6bdb4e63e31fb99c5f9e6
sudip subedi <sudipsub123@gmai.com>
2026-04-19 22:08:12 +0545

    adding file

---
+++ config.php
@@ -0,0 +1,9 @@
+// ================= config.php =================
+<?php
+$profile = [
+  "name" => "Sprasapradip",
+  "title" => "Electrical Engineer",
+  "bio" => "Specialized in electrical installation, solar systems & industrial maintenance.",
+  "cv" => "cv.pdf"
+];
+?>
