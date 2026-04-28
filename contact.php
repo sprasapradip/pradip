e1d25ec21c6250128e357bb531ee05063b0c0ec3
sprasapradip <subedipradip199@gmail.com>
2026-04-28 23:05:42 +0545

    adding file

--- contact.php
+++ contact.php
@@ -6,6 +6,66 @@ if(isset($_POST['send'])){
  $stmt->bind_param("sss",$_POST['name'],$_POST['email'],$_POST['message']);
  $msg=$stmt->execute()?"Message Sent":"Error";
 }
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$conn = new mysqli("localhost","root","","portfolio");
+
+if ($conn->connect_error) {
+    die("DB Connection failed: " . $conn->connect_error);
+}
+
+$msg = "";
+
+if(isset($_POST['send'])){
+
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Save to DB
+    $stmt = $conn->prepare("INSERT INTO messages(name,email,message) VALUES(?,?,?)");
+    $stmt->bind_param("sss", $name, $email, $message);
+    $stmt->execute();
+
+    // SMTP Email
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP config
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';  // SMTP server
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'pradipsubedi9831@gmail.com'; // your email
+        $mail->Password   = 'jgmtoakqnkbeuvtp';   // NOT normal password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+        // Sender & Receiver
+        $mail->setFrom('pradipsubedi9831@gmail.com', 'Portfolio Contact');
+        $mail->addAddress('pradipsubedi9831@gmail.com'); // where you receive
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Contact Message';
+        $mail->Body    = "
+            <h3>New Message</h3>
+            <b>Name:</b> $name <br>
+            <b>Email:</b> $email <br>
+            <b>Message:</b><br>$message
+        ";
+
+        $mail->send();
+        $msg = "Message Sent + Email Delivered";
+
+    } catch (Exception $e) {
+        $msg = "Saved but Email Failed: {$mail->ErrorInfo}";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
